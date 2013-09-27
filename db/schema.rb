dac85200d8fa4c05a57637d82130839e0f3becf4
rdias23 <rdias23@gmail.com>
2013-09-27 19:23:34 -0400

    added 100 books to the seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130926175354) do
+ActiveRecord::Schema.define(version: 20130926210844) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20130926175354) do
     t.integer  "rating_overall"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rank"
   end
 
   create_table "users", force: true do |t|
