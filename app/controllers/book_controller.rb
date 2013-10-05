88de4edfd79931aa9aa83bb478dd554e7e71a363
rdias23 <rdias23@gmail.com>
2013-10-04 20:05:59 -0400

    basic bookshelf add working

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -4,5 +4,14 @@ class BookController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user 
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.booklist_id = params[:booklist_id]
+    @book.save
+    redirect_to :controller => "home", :action => "index"
+	 flash[:notice] = "book added"
   end
 end
