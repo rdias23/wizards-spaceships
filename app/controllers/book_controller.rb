16dd09d49b7b04c0b85fbccd265ae91e09a38aa3
rdias23 <rdias23@gmail.com>
2013-10-11 10:59:48 -0400

    bunch of stuff

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,17 +1,45 @@
 class BookController < ApplicationController
+  layout 'colorbox'
   def index
   end
 
   def show
     @book = Book.find(params[:id])
     @user = current_user 
+    @booklist = Booklist.find(@user.booklist.id)
+    
+    @books_in_book_list = [] 
+    
+    @booklist.books.each do |bk|
+    	@books_in_book_list << bk.title
+    end
+		 
+    if @books_in_book_list.include? @book.title
+        @button_label = "Remove Book from Bookshelf?"
+    else
+	@button_label = "Add Book To Bookshelf?"  
+    end
+
   end
 
   def update
     @book = Book.find(params[:id])
     @booklist = Booklist.find(params[:booklist_id])
-    @book.booklists << @booklist
-    redirect_to :controller => "home", :action => "index"
-	 flash[:notice] = "book added"
+   
+    @books_in_book_list = []
+
+    @booklist.books.each do |bk|
+        @books_in_book_list << bk.title
+    end
+    
+    if @books_in_book_list.include? @book.title
+	@book.booklists = @book.booklists - [@booklist]
+   	redirect_to :controller => "book", :action => "show"
+        flash[:notice] = "book removed!" 
+    else
+    	@book.booklists << @booklist
+    	redirect_to :controller => "book", :action => "show"
+	 flash[:notice] = "book added!"
+    end
   end
 end
