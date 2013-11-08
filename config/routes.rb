40f3249e09175febb455238f626530558484753a
rdias23 <rdias23@gmail.com>
2013-11-08 18:08:19 -0500

    small fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 WizardsAndSpaceships::Application.routes.draw do
   get "home/index"
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
